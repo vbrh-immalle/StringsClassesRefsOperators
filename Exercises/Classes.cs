35d1a4adc88ba14ab6e0faa66910883aef9a4049
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2018-05-18 09:41:27 +0200

    Class Leerling + Leerling.ToString() method implementation + tests

--- Exercises/Classes.cs
+++ Exercises/Classes.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Exercises
 {
-    public class Classes
+    public class Leerling
     {
+        public string Naam { get; set; }
+        public string Klas { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Naam} ({Klas})";
+        }
     }
 }
--- StringsClassesRefsOperators/ClassesTests.cs
+++ StringsClassesRefsOperators/ClassesTests.cs
@@ -7,9 +7,13 @@ namespace StringsClassesRefsOperators
     public class ClassesTests
     {
         [Fact]
-        public void Test1()
+        public void LeerlingClassShouldHaveAWorkingToStringMethod()
         {
+            var lln = new Leerling();
+            lln.Naam = "Joske";
+            lln.Klas = "5ITN";
 
+            Assert.Equal("Joske (5ITN)", lln.ToString());
         }
     }
 }
